c67b22d7448e1fb615dc7ea001c4673e6fe19453
Samuel Berney <sam@samuelberney.com>
2018-04-28 00:20:28 -0700

    Possible main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// not implemented
+// & grab whatever libraries one might wish for
+//
+import { render } from './application.js';
+
+const main = () => {
+  render(document.querySelector('#main-content'));
+};
+
+//
+// queen of overthinking
+main();
